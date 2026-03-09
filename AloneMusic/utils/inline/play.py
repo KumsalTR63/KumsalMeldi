56d52a8b885a2a5bb194b7fe1ab7ea7194e3e0c7
KumsalTR 🇹🇷 <sikmegdgfdf@gmail.com>
2026-03-09 19:30:05 +0300

    Update play.py

--- AloneMusic/utils/inline/play.py
+++ AloneMusic/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from DeadlineTech.utils.formatters import time_to_seconds
+from AloneMusic.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
