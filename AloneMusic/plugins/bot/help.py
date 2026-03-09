715f910cdc3b987d8723f3fd9782355f362e55fa
KumsalTR 🇹🇷 <sikmegdgfdf@gmail.com>
2026-03-09 18:48:51 +0300

    Update help.py

--- AloneMusic/plugins/bot/help.py
+++ AloneMusic/plugins/bot/help.py
@@ -3,11 +3,11 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils import help_pannel
-from AnonXMusic.utils.database import get_lang
-from AnonXMusic.utils.decorators.language import LanguageStart, languageCB
-from AnonXMusic.utils.inline.help import help_back_markup, private_help_panel
+from AloneMusic import app
+from AloneMusic.utils import help_pannel
+from AloneMusic.utils.database import get_lang
+from AloneMusic.utils.decorators.language import LanguageStart, languageCB
+from AloneMusic.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
 
