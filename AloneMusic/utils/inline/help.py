822a8211e2c0e5f9343a5e942abce09beaa5fb29
KumsalTR 🇹🇷 <sikmegdgfdf@gmail.com>
2026-03-11 13:53:31 +0300

    Update help.py

--- AloneMusic/utils/inline/help.py
+++ AloneMusic/utils/inline/help.py
@@ -4,15 +4,25 @@
 
 from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import enums
 from AloneMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
-    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")]
+
+    first = [
+        InlineKeyboardButton(
+            text=_["CLOSE_BUTTON"],
+            callback_data="close",
+            style=enums.ButtonStyle.DANGER
+        )
+    ]
+
     second = [
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
             callback_data="settingsback_helper",
+            style=enums.ButtonStyle.PRIMARY
         ),
     ]
 
@@ -24,62 +34,75 @@ def help_pannel(_, START: Union[bool, int] = None):
                 InlineKeyboardButton(
                     text=_["H_B_1"],
                     callback_data="help_callback hb1",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_2"],
                     callback_data="help_callback hb2",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_3"],
                     callback_data="help_callback hb3",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text=_["H_B_4"],
                     callback_data="help_callback hb4",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_5"],
                     callback_data="help_callback hb5",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_6"],
                     callback_data="help_callback hb6",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text=_["H_B_7"],
                     callback_data="help_callback hb7",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_8"],
                     callback_data="help_callback hb8",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_9"],
                     callback_data="help_callback hb9",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text=_["H_B_10"],
                     callback_data="help_callback hb10",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_11"],
                     callback_data="help_callback hb11",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_12"],
                     callback_data="help_callback hb12",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text=_["H_B_13"],
                     callback_data="help_callback hb13",
+                    style=enums.ButtonStyle.SECONDARY
                 ),
             ],
             mark,
@@ -95,6 +118,7 @@ def help_back_markup(_):
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"],
                     callback_data="settings_back_helper",
+                    style=enums.ButtonStyle.PRIMARY
                 ),
             ]
         ]
@@ -108,6 +132,7 @@ def private_help_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_4"],
                 url=f"https://t.me/{app.username}?start=help",
+                style=enums.ButtonStyle.SUCCESS
             ),
         ],
     ]
