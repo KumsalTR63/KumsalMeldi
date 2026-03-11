2ee7fe6763ac412aaf2332bfc3d1d5b1329366ab
KumsalTR63 <266187723+KumsalTR63@users.noreply.github.com>
2026-03-11 11:53:35 +0000

    𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ + #⟶̽ जय श्री ༢།म >𝟑🙏🚩

--- AloneMusic/utils/inline/play.py
+++ AloneMusic/utils/inline/play.py
@@ -1,6 +1,8 @@
-from AloneMusic import app
 import math
+
 from pyrogram.types import InlineKeyboardButton
+
+from AloneMusic import app
 from AloneMusic.utils.formatters import time_to_seconds
 
 
@@ -88,9 +90,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")
-        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
