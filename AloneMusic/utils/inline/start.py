a601ea1f06b26d85178762f282a5ff761e3085a5
KumsalTR 🇹🇷 <sikmegdgfdf@gmail.com>
2026-03-11 14:08:41 +0300

    Update start.py

--- AloneMusic/utils/inline/start.py
+++ AloneMusic/utils/inline/start.py
@@ -1,4 +1,3 @@
-from pyrogram import enums
 from pyrogram.types import InlineKeyboardButton
 
 import config
@@ -10,13 +9,11 @@ def start_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-                style=enums.ButtonStyle.PRIMARY
+                url=f"https://t.me/{app.username}?startgroup=true"
             ),
             InlineKeyboardButton(
                 text=_["S_B_2"],
-                url=config.SUPPORT_CHAT,
-                style=enums.ButtonStyle.SUCCESS
+                url=config.SUPPORT_CHAT
             ),
         ],
     ]
@@ -28,39 +25,33 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-                style=enums.ButtonStyle.PRIMARY
+                url=f"https://t.me/{app.username}?startgroup=true"
             )
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_4"],
-                callback_data="settings_back_helper",
-                style=enums.ButtonStyle.SECONDARY
+                callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_5"],
-                user_id=config.OWNER_ID,
-                style=enums.ButtonStyle.SUCCESS
+                user_id=config.OWNER_ID
             ),
             InlineKeyboardButton(
                 text=_["S_B_2"],
-                url=config.SUPPORT_CHAT,
-                style=enums.ButtonStyle.SUCCESS
+                url=config.SUPPORT_CHAT
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_6"],
-                url=config.SUPPORT_CHANNEL,
-                style=enums.ButtonStyle.PRIMARY
+                url=config.SUPPORT_CHANNEL
             ),
             InlineKeyboardButton(
                 text=_["S_B_7"],
-                url=config.UPSTREAM_REPO,
-                style=enums.ButtonStyle.DANGER
+                url=config.UPSTREAM_REPO
             ),
         ],
     ]
